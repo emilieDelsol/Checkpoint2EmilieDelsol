ad29e5d7ba34d4789efe8ae0b32755cc24939680
emilieDelsol <emiliedelsol1@gmail.com>
2020-11-17 10:38:26 +0100

    ecriture requete dans ssms

--- BDDcheckPoint2EmilieD.sql
+++ BDDcheckPoint2EmilieD.sql
@@ -31,3 +31,22 @@ GO
 INSERT INTO Student (StudentFirstName,StudentLastName, FK_PromotionId)
 	VALUES ('Emilie', 'Delsol',1),('Maïlys','Dumas',1),('Colas','Durcy',1),('Hervé','Meste',1),('Adrien','Zapico',1),
 			('Emilie', 'Anglade',2),('Rooarii','Manuel',2),('Lisa-Lou', 'Kara',2),('Jessica','Giraud',2),('Flavien','Besseau',2)
+
+INSERT INTO Controle (CotroleNote,FK_StudentId)
+	VALUES (20,1),(15,1),(16,1),(17,1),(12,1),(15,1),(18,1),
+			(20,2),(15,2),(16,2),(17,2),(12,2),(15,2),(18,2),
+			(20,3),(15,3),(16,3),(7,3),(12,3),(15,3),(18,3),
+			(20,4),(15,4),(16,4),(17,4),(12,4),(15,4),(18,4),
+			(20,5),(15,5),(16,5),(17,5),(12,5),(15,5),(18,5),
+			(20,6),(15,6),(6,6),(17,6),(12,6),(15,6),(18,6),
+			(20,7),(15,7),(16,7),(1,7),(12,7),(15,7),(18,7),
+			(15,8),(15,8),(16,8),(15,8),(12,8),(15,8),(18,8),
+			(20,9),(15,9),(16,9),(17,9),(10,9),(15,9),(18,9),
+			(20,10),(15,10),(16,10),(13,10),(12,10),(15,10),(18,10)
+
+
+
+SELECT  Student.StudentLastName AS "Nom" , Student.StudentFirstName AS "Prénom", AVG(Controle.CotroleNote) AS "moyenne de l'étudiant: "
+	from Controle INNER JOIN Student  ON StudentId=FK_StudentId
+		GROUP BY StudentId,StudentLastName, StudentFirstName
+
